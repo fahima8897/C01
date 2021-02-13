ee7a49ad933a2b612fce426e6d9fd3403d6982a4
Fahima Boumellil <fahimaboumellil@MacBook-Air-de-Fahima.local>
2021-02-13 23:23:12 +0100

    a

---
+++ ex07/fr_rev_int_tab.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int i;
+	int ri;
+	int swap;
+
+	i = 0;
+	ri = size - 1;
+	while (i < (size / 2))
+	{
+		swap = tab[i];
+		tab[i] = tab[ri];
+		tab[ri] = swap;
+		i++;
+		ri--;
+	}
+
+
+}
